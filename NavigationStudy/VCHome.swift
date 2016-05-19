31777043e0d972279d03269434ec58e9e6bb1d5d
saumya <2saumyaray@gmail.com>
2016-05-19 13:17:55 +0530

    navigation

--- NavigationStudy/VCHome.swift
+++ NavigationStudy/VCHome.swift
@@ -13,11 +13,15 @@ class VCHome: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("VCHome : viewDidLoad")
+        //showMenu()
+        showUserHome()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        print("VCHome : didReceiveMemoryWarning")
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -29,6 +33,14 @@ class VCHome: UIViewController {
     @IBAction func showMenu(){
         print("VCHome : showMenu")
         self.performSegueWithIdentifier("segue_toMenu", sender: self)
+    }
+    private func showUserHome(){
+        moveToViewControllerWithStoryboardID("sid_user_home")
+    }
+    private func moveToViewControllerWithStoryboardID(sid:String){
+        let storyboard = UIStoryboard(name:"Main",bundle: nil)
+        let vc = storyboard.instantiateViewControllerWithIdentifier(sid)
+        self.navigationController?.showViewController(vc, sender: self)
     }
 
 
